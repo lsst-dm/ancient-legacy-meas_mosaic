8d7c51b6c1adac70388d7387035b45a7670c1b59
Perry Gee <pgee@physics.ucdavis.edu>
2012-10-18 06:54:32 -0700

    Version of bug linked to GSL

--- tests/gslhang.cc
+++ tests/gslhang.cc
@@ -2,26 +2,23 @@
 #include <math.h>
 #include <vector>
 #include <ctime>
+#include <iostream>
 #include <strings.h>
 #include "fitsio.h"
 
 #include <gsl/gsl_linalg.h>
-#include "lsst/meas/mosaic/mosaicfit.h"
 
-using namespace lsst::meas::mosaic;
-
-using namespace lsst::meas::mosaic;
-using namespace lsst::afw::detection;
 
 double* solveMatrix_GSL(long size, double *a_data, double *b_data) {
-    std::cout << "size: " <<size << std::endl;
+    std::cout << "Calling solveMatrix, size = " <<size << std::endl;
+/*  std::cout << "size: " <<size << std::endl;
     std::cout << "a_data" << std::endl;
     for (int i=0; i < size; i++) 
         std::cout <<i << ": " << a_data[i] << std::endl;
     std::cout << "b_data" << std::endl;
     for (int i=0; i < size; i++) 
         std::cout <<i << ": " << b_data[i] << std::endl;
-
+*/
     gsl_matrix_view a = gsl_matrix_view_array(a_data, size, size);
     gsl_vector_view b = gsl_vector_view_array(b_data, size);
     double *c_data = new double[size];
@@ -36,31 +33,28 @@ double* solveMatrix_GSL(long size, double *a_data, double *b_data) {
     gsl_linalg_LU_solve(&a.matrix, p, &b.vector, &c.vector);
     std::cout << "Calling free"   << std::endl;
     gsl_permutation_free(p);
-    std::cout << "Returning"   << std::endl;
 
     return c_data;
 }
 int main(int argc, char **argv)
 {
     char buf[BUFSIZ];
+    char * str;
     int icnt;
     double var;
     FILE *fp = fopen("gslhang.dat", "rt");
-    fgets(buf, BUFSIZ, fp);
-    sscanf(buf, "%d", &icnt);
-    std::cout << icnt << std::endl;
+    str = fgets(buf, BUFSIZ, fp);
+    sscanf(str, "%d", &icnt);
     double *a_data = new double[icnt*icnt];
     double *b_data = new double[icnt];
     for (int i = 0; i < icnt*icnt; i++) {
-	fgets(buf, BUFSIZ, fp);
-	sscanf(buf, "%lf", &var);
-        //#std::cout << var << std::endl;
+	str = fgets(buf, BUFSIZ, fp);
+	sscanf(str, "%lf", &var);
         a_data[i] = var;
     }
     for (int i = 0; i < icnt; i++) {
-	fgets(buf, BUFSIZ, fp);
-	sscanf(buf, "%lf", &var);
-        //#std::cout << var << std::endl;
+	str = fgets(buf, BUFSIZ, fp);
+	sscanf(str, "%lf", &var);
         b_data[i] = var;
     }
     solveMatrix_GSL(icnt, a_data, b_data);
